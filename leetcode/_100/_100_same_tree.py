30ed4a1c3a5e81d07caa15818b4956dc49e53f3b
Vlad Povarna <povarna@adobe.com>
2023-01-10 10:46:33 +0200

    [100] Same Tree problem

---
+++ leetcode/_100/_100_same_tree.py
@@ -0,0 +1,16 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val, left: None, right: None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return p == q
