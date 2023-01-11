3689a50157787ccbda8f6b72890f702806506e6a
Vlad Povarna <povarna@adobe.com>
2023-01-11 10:20:51 +0200

    [15] 3Sum problem

---
+++ leetcode/_100/_15_3Sum.py
@@ -0,0 +1,21 @@
+from typing import List, Set
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = set()
+        for i, v in enumerate(nums):
+            self.twoSum(nums[i + 1:], -v, ans)
+        return list(ans)
+
+    def twoSum(self, nums, target, ans):
+        visited = set()
+        for n in nums:
+            if target - n in visited:
+                ans.add((n, target - n, -target))
+            visited.add(n)
+
+
+if __name__ == '__main__':
+    print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
