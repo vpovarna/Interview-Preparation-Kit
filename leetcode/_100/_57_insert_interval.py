a4296679bb95c02ae6266b13a103a40f3cc5a729
Vlad Povarna <povarna@adobe.com>
2023-01-25 10:26:06 +0200

    @trivial refactor insert interval problem

--- leetcode/_100/_57_insert_interval.py
+++ leetcode/_100/_57_insert_interval.py
@@ -2,23 +2,23 @@ from typing import List
 
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
         result = []
         i = 0
         for i, (x, y) in enumerate(intervals):
-            if y < newInterval[0]:
+            if y < new_interval[0]:
                 result.append([x, y])
-            elif newInterval[1] < x:
+            elif new_interval[1] < x:
                 i -= 1
                 break
             else:
-                newInterval[0] = min(newInterval[0], x)
-                newInterval[1] = max(newInterval[1], y)
+                new_interval[0] = min(new_interval[0], x)
+                new_interval[1] = max(new_interval[1], y)
 
-        return result + [newInterval] + intervals[i + 1:]
+        return result + [new_interval] + intervals[i + 1:]
 
 
 if __name__ == '__main__':
-    # print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
-    # print(Solution().insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
+    print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
+    print(Solution().insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
     print(Solution().insert([[1, 5]], [2, 3]))
