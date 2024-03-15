601abd179e58db8b86dcb016b1fdc966e4f4adf4
povarna <vlad.povarna@uipath.com>
2024-03-15 09:20:30 +0200

    Find the Pivot Integer problem

---
+++ leetcode/_2500/_2485_find_the_pivot_integer.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        x = sqrt(n * (n + 1) / 2)
+
+        if x % 1 != 0:
+            return -1
+        else:
+            return int(x)
