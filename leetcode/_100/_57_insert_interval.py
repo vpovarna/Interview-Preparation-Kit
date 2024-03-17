98c22e7be2367d26fd0e192a8756b5ee18af7f17
povarna <vlad.povarna@uipath.com>
2024-03-17 14:56:59 +0200

    Daily problem

--- leetcode/_100/_57_insert_interval.py
+++ leetcode/_100/_57_insert_interval.py
@@ -17,6 +17,24 @@ class Solution:
 
         return result + [new_interval] + intervals[i + 1:]
 
+    def insertV2(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        merged = []
+        i = 0
+
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
+        
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+            i += 1
+        merged.append(newInterval)
+
+        while i < len(intervals):
+            merged.append(intervals[i])
+            i += 1
+        return merged
+
 
 if __name__ == '__main__':
     print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
