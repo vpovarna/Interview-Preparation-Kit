4670052a70e4285bf58edafca00d126fcef1944d
povarna <vlad.povarna@uipath.com>
2024-05-10 20:30:56 +0300

    Daily problem

---
+++ leetcode/_800/_786_K-th_smallest_prime_fraction.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: list[int], k: int) -> list[int]:
+        n = len(arr)
+        
+        heap = [(arr[r]/arr[n-1], r, n-1) for r in range(n)]
+        heapq.heapify(heap)
+
+        for _ in range(k-1):
+            val, r, c = heapq.heappop(heap)
+            if c :
+                heapq.heappush(heap, (arr[r]/arr[c-1], r, c-1))
+
+        return arr[heap[0][1]], arr[heap[0][2]]
